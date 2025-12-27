f9c31cc370e2ec2ef7cd333c2446f048628faa7b
Vishal Bhardwaj <156419780+visz29@users.noreply.github.com>
2025-12-28 00:12:46 +0530

    update 12.07 am 28/dec/2025

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import V0 from './V0.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Share />
+    {/* <Share /> */}
     {/* <V0 /> */}
   </React.StrictMode>,
 )
